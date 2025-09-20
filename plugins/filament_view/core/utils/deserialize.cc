381abfbfca4c6452e8bfdd177e8278b0762a86c9
Joel Winarske <joel.winarske@gmail.com>
2025-09-20 09:13:45 -0700

    Fix clang-tidy error
    Signed-off-by: Joel Winarske <joel.winarske@gmail.com>

--- plugins/filament_view/core/utils/deserialize.cc
+++ plugins/filament_view/core/utils/deserialize.cc
@@ -154,18 +154,14 @@ void Deserialize::DecodeParameterWithDefaultInt64(
   const flutter::EncodableMap& params,
   const int64_t& default_value
 ) {
-
-  if (const auto it = params.find(flutter::EncodableValue(key)); it == params.end()) {
+  if (const auto it = params.find(flutter::EncodableValue(key));
+      it == params.end() || std::holds_alternative<std::monostate>(it->second)) {
     *out_value = default_value;
   } else if (std::holds_alternative<int64_t>(it->second)) {
     *out_value = std::get<int64_t>(it->second);
   } else if (std::holds_alternative<int32_t>(it->second)) {
     *out_value = std::get<int32_t>(it->second);
-  } else if (std::holds_alternative<std::monostate>(it->second)) {
-    *out_value = default_value;
   } else {
-    // get type as string
-    // auto type = std::string(it->second.type().name());
     spdlog::debug("Unhandled type for key: {}", key);
     *out_value = default_value;
   }
