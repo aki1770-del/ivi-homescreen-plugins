e442b954aa677581b402328b152e7473d7feb33e
AhmedAdelWafdy7 <fandes99@gmail.com>
2026-02-12 16:06:42 +0000

    fix clang-format for pigeon

--- plugins/flatpak/messages.g.cc
+++ plugins/flatpak/messages.g.cc
@@ -1139,7 +1139,8 @@ void FlatpakApi::SetUp(flutter::BinaryMessenger* binary_messenger,
                 return;
               }
               EncodableList wrapped;
-              wrapped.emplace_back(EncodableValue(std::move(output).TakeValue()));
+              wrapped.emplace_back(
+                  EncodableValue(std::move(output).TakeValue()));
               reply(EncodableValue(std::move(wrapped)));
             } catch (const std::exception& exception) {
               reply(WrapError(exception.what()));
